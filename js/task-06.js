5393b23f341f30c62a3697ffed2b171b422c3078
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-02-23 23:28:44 +0200

    task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', onInputBlur);
+
+function onInputBlur (event){
+if (Number(inputEl.dataset.length) === event.currentTarget.value.length) {
+inputEl.classList.add('valid');
+inputEl.classList.remove('invalid');   
+}else{ 
+inputEl.classList.remove('valid');
+inputEl.classList.add('invalid');
+}
+}
+
