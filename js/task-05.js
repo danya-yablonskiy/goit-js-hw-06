e46fd819038360e70168470b78120c6f7992eed8
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-02-23 22:23:03 +0200

    task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#name-input');
+const spanEl = document.querySelector('#name-output');
+
+
+inputEl.addEventListener('input', enterName);
+
+
+function enterName(event){
+  if (event.currentTarget.value === '')  {
+    spanEl.textContent = 'Anonymous'
+  }else{
+ spanEl.textContent = event.currentTarget.value;
+  }
+    }
