9a3b8f5f0308d15339e1a3b008d27fe723b0ded8
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-02-25 00:25:34 +0200

    task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,9 +7,11 @@ const ingredients = [
   'Condiments',
 ];
 const list = document.querySelector('#ingredients')
+const elements = [];
 ingredients.forEach(value =>{
 const listItem = document.createElement('li');
 listItem.textContent = value;
 listItem.classList = 'item';
-list.append(listItem);
+elements.push(listItem);
 })
+list.append(...elements);
