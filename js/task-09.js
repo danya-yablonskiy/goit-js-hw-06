31ba9891644a3cf61a3e3154f70f7b1533296022
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-02-24 14:58:02 +0200

    task-09.js

---
+++ js/task-09.js
@@ -0,0 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyEl = document.querySelector('body');
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', changeColor);
+
+function changeColor(event){
+bodyEl.style.backgroundColor = getRandomHexColor();
+spanEl.textContent = getRandomHexColor();
+}
