7dfae8c6d6da5726a5de4268108d4ccbc506f964
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-02-24 22:15:51 +0200

    task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,21 @@
+
+// const listEl = document.querySelector('#categories');
+// const itemEl = document.querySelectorAll('.item');
+
+// console.log(`Number of categories: ${itemEl.length}`);
+
+// listEl.forEach(element => {
+//     console.log(`Category: ${listEl.firstElementChild.textContent}`);
+//     console.log(`Elements: ${listEl.lastElementChild.length}`);
+// });
+
+
+const listEl = document.querySelector('#categories');
 const itemEl = document.querySelectorAll('.item');
+
 console.log(`Number of categories: ${itemEl.length}`);
 
-const titleEl = document.querySelectorAll('.item > h2');
-const subList = document.querySelectorAll('.item > ul');
-
-for (let i = 0; i < subList.length; i++) {
-console.log(`Category: ${titleEl[i].textContent}`);
-console.log(`Elements: ${subList[i].children.length}`);
-}
+itemEl.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
