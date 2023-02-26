47c9eeca2ead990222ee1a8b54b390c25b9b3bca
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-02-26 19:13:00 +0200

    task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -11,7 +11,7 @@ const boxEl = document.querySelector('#boxes');
 
 
 
-const arrayEl = [];
+
 createBtnEl.addEventListener('click', onBtnClickCreateAmount);
 
 function onBtnClickCreateAmount () {
@@ -19,13 +19,19 @@ function onBtnClickCreateAmount () {
 }
 
 function createBoxes(amount){
-for (let i = 0; i < amount; i++) {
+  const arrayEl = [];
+for (let i = 1; i <= amount; i++) {
   const divEl = document.createElement('div');
-  divEl.width = `${20+amount*10}px`;
-  divEl.height = `${20+amount*10}px`;
+  divEl.style.width = `${20+i*10}px`;
+  divEl.style.height = `${20+i*10}px`;
   divEl.style.backgroundColor = getRandomHexColor();
   arrayEl.push(divEl);
 }
  boxEl.append(...arrayEl);
 }
 
+destroyBtlEl.addEventListener('click', destroyBoxes);
+
+function destroyBoxes(){
+  boxEl.innerHTML = '';
+}
